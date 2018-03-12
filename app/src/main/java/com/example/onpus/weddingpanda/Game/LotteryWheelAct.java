a4b4e14d8f0b54c23830dcf98a09f60323618951
DESKTOP-MBO6HON\onpus <onpusnow@gmail.com>
2018-03-12 22:06:04 +0800

    before circle

---
+++ app/src/main/java/com/example/onpus/weddingpanda/Game/LotteryWheelAct.java
@@ -0,0 +1,13 @@
+package com.example.onpus.weddingpanda.Game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LotteryWheelAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lottery_wheel);
+    }
+}
